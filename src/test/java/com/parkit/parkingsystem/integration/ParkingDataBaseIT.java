b7c860a6d66e964652b86cd1620c4b4978a2a40d
w-boar <av.w.boar@gmail.com>
2021-07-07 10:30:21 +0200

    Revert "2 x TODO IT"
    This reverts commit 48b96b65

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,7 +4,6 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
-import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -14,9 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,30 +47,19 @@ public class ParkingDataBaseIT {
 
     }
 
-    @Test //check that a ticket is actually saved in DB and Parking table is updated with availability
+    @Test
     public void testParkingACar(){
-        //GIVEN
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        //WHEN
         parkingService.processIncomingVehicle();
-        Ticket ticket = ticketDAO.getTicket("ABCDEF");
-        //THEN
-        assertEquals( "ABCDEF", ticket.getVehicleRegNumber());
-        assertEquals(false, ticket.getParkingSpot().isAvailable());
+        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
     }
 
-    @Test //check that the fare generated and out time are populated correctly in the database
+    @Test
     public void testParkingLotExit(){
-        //GIVEN
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        //WHEN
         parkingService.processExitingVehicle();
-        Ticket ticket = ticketDAO.getTicket("ABCDEF");
-        //THEN
-        assertNotNull(ticket.getOutTime());
-        assertNotNull(ticket.getPrice());
-
+        //TODO: check that the fare generated and out time are populated correctly in the database
     }
 
 }
